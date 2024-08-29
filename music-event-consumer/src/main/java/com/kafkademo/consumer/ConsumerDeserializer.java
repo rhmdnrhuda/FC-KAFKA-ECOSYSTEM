c1df7e287ddc7f56175c9cdcee3693af882a7997
Rahmad Nurhuda <rhmdnrhuda@gmail.com>
2024-08-29 20:30:51 +0700

    implement logging using elasticstack

---
+++ music-event-consumer/src/main/java/com/kafkademo/consumer/ConsumerDeserializer.java
@@ -0,0 +1,31 @@
+package com.kafkademo.consumer;
+
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumerDeserializer {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "my-group");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("my-topic"));
+
+        while (true) {
+            var records = consumer.poll(1000);
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.printf("Offset = %d, Key = %s, Value = %s%n", record.offset(), record.key(), record.value());
+            }
+        }
+    }
+}
---
+++ music-event-producer/src/main/java/com/kafkademo/producer/ProduceSerializer.java
@@ -0,0 +1,26 @@
+package com.kafkademo.producer;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProduceSerializer {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+
+        for (int i = 0; i < 10; i++) {
+            producer.send(new ProducerRecord<>("my-topic", "Message " + i));
+        }
+
+        producer.close();
+    }
+}
